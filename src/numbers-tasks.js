65397dea9c3b439ac7fae13d5c5a8dea38a8079b
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2023-12-24 16:11:46 +0200

    3

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -226,8 +226,12 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const convertedValue = Number(value);
+  if (!isNaN(convertedValue)) {
+    return convertedValue;
+  }
+  return def;
 }
 
 /**
