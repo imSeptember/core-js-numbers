15244d83433befc4e61be3f9152b0d9858248122
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2023-12-24 15:12:02 +0200

    1

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -95,8 +95,8 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(a, b) {
- return -b / a;
+function getLinearEquationRoot(/* a, b */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -116,8 +116,12 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const dotProduct = x1 * x2 + y1 * y2;
+  const magnitude1 = Math.sqrt(x1 * x1 + y1 * y1);
+  const magnitude2 = Math.sqrt(x2 * x2 + y2 * y2);
+  const angle = Math.acos(dotProduct / (magnitude1 * magnitude2));
+  return angle;
 }
 
 /**
