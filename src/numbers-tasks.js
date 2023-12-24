c139aafef9ef36e8378605dc855bd47eaa7314c2
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2023-12-24 15:48:03 +0200

    1

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -466,8 +466,9 @@ function isInteger(number) {
  * '4.567abcdefgh' => 4.567
  * 'abcdefgh'      => NaN
  */
-function getFloatOnString(/* str */) {
-  throw new Error('Not implemented');
+function getFloatOnString(str) {
+  const parsedFloat = parseFloat(str);
+  return isNaN(parsedFloat) ? NaN : parsedFloat;
 }
 
 /**
