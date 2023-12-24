386f3b49c57916f22d9b5a0d402ed0d22ddfd35e
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2023-12-24 16:10:48 +0200

    1

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -184,8 +184,10 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const divisor = 10 ** pow;
+  const roundedResult = Math.round(num / divisor);
+  return roundedResult * divisor;
 }
 
 /**
