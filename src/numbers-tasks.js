342eee7b454d47e8b2eb5fb281915f678e26be71
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2023-12-24 16:01:04 +0200

    1

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -577,8 +577,8 @@ function getIntegerPartNumber(number) {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(x1, x2, x3) {
-  return x1 + x2 + x3;
+function getSumOfNumbers(/* x1, x2, x3 */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -609,8 +609,8 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(/* min, max */) {
-  throw new Error('Not implemented');
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /**
